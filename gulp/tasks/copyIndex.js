3c597058b445655e9b69a16db992f0ef5c655013
Micah Roberson <micah.roberson@gmail.com>
2015-06-27 11:23:50 -0700

    Add and comment out logic to duplicate index.html for static hosting.

--- gulp/tasks/copyIndex.js
+++ gulp/tasks/copyIndex.js
@@ -1,10 +1,25 @@
 'use strict';
 
 var gulp   = require('gulp');
+var rename = require('gulp-rename');
 var config = require('../config');
 
 gulp.task('copyIndex', function() {
 
   gulp.src(config.sourceDir + 'index.html').pipe(gulp.dest(config.buildDir));
-
+  // gulp.src(config.sourceDir + 'index.html')
+  //   .pipe(rename('wedding.html'))
+  //   .pipe(gulp.dest(config.buildDir));
+  // gulp.src(config.sourceDir + 'index.html')
+  //   .pipe(rename('accomodations.html'))
+  //   .pipe(gulp.dest(config.buildDir));
+  // gulp.src(config.sourceDir + 'index.html')
+  //   .pipe(rename('mill-valley-and-recommendations.html'))
+  //   .pipe(gulp.dest(config.buildDir));
+  // gulp.src(config.sourceDir + 'index.html')
+  //   .pipe(rename('schedule.html'))
+  //   .pipe(gulp.dest(config.buildDir));
+  // gulp.src(config.sourceDir + 'index')
+  //   .pipe(rename('registry.html'))
+  //   .pipe(gulp.dest(config.buildDir));
 });
